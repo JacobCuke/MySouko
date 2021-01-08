f3cc8efb8f86a37822a0cba4d5e94319f2aeb9d4
Jacob Cuke <jacobcuke@gmail.com>
2021-01-08 13:50:27 -0700

    Resize large uploaded images before saving

--- souko/models.py
+++ souko/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import date
+from PIL import Image
 
 class Item(models.Model):
     ANIME = 'AN'
@@ -23,3 +24,13 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.width > 300 or img.height > 500:
+            output_size = (300, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
