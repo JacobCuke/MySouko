95d836f434b94f9afe967d03b426c17e01b42243
Jacob Cuke <jacobcuke@gmail.com>
2021-02-16 12:58:41 -0700

    Test that correct settings.py file is being used

--- MySouko/settings.py
+++ MySouko/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SOUKO_KEY')
+print("I HAVE BEEN LOADED!!!!!!!!!!!!!!!!#############")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
