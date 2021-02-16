57a85f90781c3dc58fbcd9524f9617f8e0227b1c
Jacob Cuke <jacobcuke@gmail.com>
2021-02-16 13:02:41 -0700

    Test if secret key environment variable is loading properly

--- MySouko/settings.py
+++ MySouko/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SOUKO_KEY')
-print("I HAVE BEEN LOADED!!!!!!!!!!!!!!!!#############")
+SECRET_KEY = os.environ.get('DJANGO_SOUKO_KEY', "12345")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
